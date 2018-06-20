c16ebc49e9b6196ccf9b2a29eb43ee8b7efe632d
johnale <johnale@cisco.com>
2018-06-20 01:35:04 -0400

    added base for todos api code

---
+++ server/db/mongoose.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+
+
+//connect to mongoose
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+
+module.exports = {
+  mongoose
+};
